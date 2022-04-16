6c2611b2254c78f639ba0a57f400ef770a9fb23f
a951087423 <v0973570232@gmail.com>
2022-04-16 22:05:13 +0800

    add programming

---
+++ datatype.py
@@ -0,0 +1,39 @@
+#資料: 程式的基本單位
+
+#字串
+"測試中文"
+"Hello World"
+
+#布林值
+True    #正確
+False   #錯誤
+
+#有順序、可變動的列表 List
+[3,4,5]
+["Hello" , "World"]
+
+#有順序、不可變動的列表 Tuple
+(3,4,5)
+("Hello" , "World")
+
+#集合 Set
+{3,4,5}
+{"Hello" , "World"}
+
+#字典 Dictionary
+{"apple":"蘋果" , "data":"資料"}
+
+#變數: 用來儲存資料的自訂名稱
+data=42           #變數名稱=資料
+henry="handsome"
+print(42)         #print(資料)
+print(True)
+print(data)
+print(henry)
+print([1,2,3])
+data=87           #取代舊的資料
+print(data)
+data="Apex is a good game. I like it"
+print(data)       #取代舊的資料2
+data={5,12,13}
+print(data)
---
+++ start.py
@@ -0,0 +1,5 @@
+# 註解:寫給人看的 用來做紀錄，說明
+# 第一隻程式
+# 撰寫程式檔案，副檔名使用py
+# run file: python file_name
+print("Hello python")
